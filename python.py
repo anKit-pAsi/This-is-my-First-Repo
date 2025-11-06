21b028a5add8c53dc61d13c7095f3623a8747bfc
Ankit Pasi <ankitpasi628@gmail.com>
2025-11-06 20:27:16 +0530

    Include features

--- python.py
+++ python.py
@@ -1,10 +1,13 @@
 print("Simple Calculator Program!")
 
+
 # Here calculator Simple Program
+
+# Calculator Extra Function
 
 user = int(input("Enter first number: "))
 user1 = int(input("Enter second number: "))
-operators = input("Choose Symbol + - * /: ")
+operators = input("Choose Symbol + - * / // % **: ")
 
 if operators == "+":
     print(user + user1)
@@ -17,6 +20,15 @@ elif operators == "*":
 
 elif operators == "/":
     print(user / user1)
+
+elif operators == "//":
+    print(user // user1)
+
+elif operators == "%":
+    print(user % user1)
+
+elif operators == "**":
+    print(user ** user1)
 
 else:
     print("Invalid Symbol!")
