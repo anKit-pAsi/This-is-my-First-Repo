9a6397409e05edf31a4a4a8612358cf405b3fee3
Ankit Pasi <ankitpasi628@gmail.com>
2025-11-06 20:15:05 +0530

    Hello Python

---
+++ python.py
@@ -0,0 +1,22 @@
+print("Simple Calculator Program!")
+
+# Here calculator Simple Program
+
+user = int(input("Enter first number: "))
+user1 = int(input("Enter second number: "))
+operators = input("Choose Symbol + - * /: ")
+
+if operators == "+":
+    print(user + user1)
+
+elif operators == "-":
+    print(user - user1)
+
+elif operators == "*":
+    print(user * user1)
+
+elif operators == "/":
+    print(user / user1)
+
+else:
+    print("Invalid Symbol!")
