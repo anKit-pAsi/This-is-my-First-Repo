f525d2f7d81267fd18da04126c0c6c8e23c28d96
anKit-pAsi <ankitpasi628@gmail.com>
2025-11-06 21:56:44 +0530

    Normal Update

--- python.py
+++ python.py
@@ -7,38 +7,33 @@ while True:
 
 # Calculator Extra Function
 
-user = int(input("Enter first number: "))
-user1 = int(input("Enter second number: "))
-operators = input("Choose Symbol + - * /: ")
+    user = int(input("Enter first number: "))
+    user1 = int(input("Enter second number: "))
+    operators = input("Choose Symbol + - * /: ")
 
-if operators == "+":
-    print(user + user1)
+    if operators == "+":
+        print(user + user1)
 
-elif operators == "-":
-    print(user - user1)
+    elif operators == "-":
+        print(user - user1)
 
-elif operators == "*":
-    print(user * user1)
+    elif operators == "*":
+        print(user * user1)
 
-elif operators == "/":
-    print(user / user1)
+    elif operators == "/":
+        print(user / user1)
 
-else:
-    print("Invalid Symbol!")
+    else:
+        print("Invalid Symbol!")
 
 # User Voting Syestem
 
-print(f"{'*#######*':>20}{'User Voting System'}{'*#######*'}\n")
+    print(f"{'*#######*':>20}{'User Voting System'}{'*#######*'}\n")
 
-user  = int(input(f"{'Enter your age: ':>30}"))
+    user  = int(input(f"{'Enter your age: ':>30}"))
 
-if user >= 18:
-    print(f"{'You Eligible To Vote':>33}")
+    if user >= 18:
+        print(f"{'You Eligible To Vote':>33}")
 
-else:
-    print(f"{'Not Eligible':>28}")
-
-
-
-
-
+    else:
+        print(f"{'Not Eligible':>28}")
